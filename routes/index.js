f4c0d3f5c34feb66cc408fe673ccc71e664e5593
DMickulesku <danmickulesku@gmail.com>
2017-10-29 17:43:34 -0600

    runner and event routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,107 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex')
-var cors = require('cors')
-var bodyParser = require('body-parser')
+// var cors = require('cors')
+// var bodyParser = require('body-parser')
 
-router.use(cors())
-router.use(bodyParser.urlencoded({extended: false}))
-router.use(bodyParser.json())
+// router.use(cors())
+// router.use(bodyParser.urlencoded({extended: false}))
+// router.use(bodyParser.json())
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('./', function (req, res) {
-  knex('')
+//Routes for runner table
+router.get('/', function (req, res) {
+  knex('runner')
+    .then((runner) => {
+      res.json(runner)
+    })
 })
+
+router.get('/:id', (req, res) => {
+  let id = req.params.id
+  knex('runner')
+    .where('id', id)
+    .then((runner) => {
+      res.json(runner)
+    })
+})
+
+router.post('/', (req, res) => {
+  let post = req.body
+  knex('runner')
+  .insert(post)
+  .returning('*')
+  .then((runner) => {
+    res.json(runner)
+  })
+})
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id
+  let edit = req.body
+  knex('runner').where('id', id)
+  .update(edit)
+  .returning('*')
+  .then(edited => {
+    res.json(edited)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id
+  knex('runner')
+    .where('id', id).del().returning("*")
+    .then(deleted => {
+      res.json({deleted})
+  })
+
+})
+
+//Routes for event table
+router.get('/', function (req, res) {
+  knex('event')
+    .then((event) => {
+      res.json(event)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  let id = req.params.id
+  knex('event')
+    .where('id', id)
+    .then((event) => {
+      res.json(event)
+    })
+})
+
+router.post('/', (req, res) => {
+  let post = req.body
+  knex('event')
+  .insert(post)
+  .returning('*')
+  .then((event) => {
+    res.json(event)
+  })
+})
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id
+  let edit = req.body
+  knex('event').where('id', id)
+  .update(edit)
+  .returning('*')
+  .then(edited => {
+    res.json(edited)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id
+  knex('event')
+    .where('id', id).del().returning("*")
+    .then(deleted => {
+      res.json({deleted})
+  })
+})
+
 
 module.exports = router;
